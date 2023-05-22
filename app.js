40cde1b325d89ae3e51c49b6cbb1b4f4ac49a24f
IvanUkraine1 <118217807+IvanUkraine1@users.noreply.github.com>
2023-05-22 23:02:07 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+const header = document.querySelector('.header');
+const gallery = document.querySelector('.gallery');
+const history = document.querySelector('.history');
+const menu = document.querySelector('.menu');
+
+const navLinks = document.querySelectorAll('.nav-item');
+const headerLinks = document.querySelectorAll('.header-bth');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault(); 
+    const target = document.querySelector(link.getAttribute('href'));
+
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+headerLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault(); 
+    const target = document.querySelector(link.getAttribute('href'));
+
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+const scrollTopBtn = document.querySelector('#scroll-top-btn');
+
+window.addEventListener('scroll', () => {
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+scrollTopBtn.style.display = 'block';
+} else {
+scrollTopBtn.style.display = 'none';
+}
+});
+
+scrollTopBtn.addEventListener('click', () => {
+window.scrollTo({
+top: 0,
+behavior: 'smooth'
+});
+});
+
+const year = new Date().getFullYear();
+const footerText = document.querySelector('.footer-text');
+footerText.innerHTML = `© ${year} Ваша компанія. Всі права захищені.`;
